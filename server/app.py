b671c2c38aac95a68ec4f2f18a9f4590c0a35f65
Anthony <anthony.onyango@student.moringaschool.com>
2024-10-08 23:26:25 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,8 +1,19 @@
-#!/usr/bin/env python3
-
-from flask import Flask
+import os
+from flask import Flask, current_app, g, request,make_response
 
 app = Flask(__name__)
+@app.before_request
+def app_path():
+    g.path = os.path.abspath(os.getcwd())
 
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    appname = current_app.name
+    response = make_response(f'''<h1>The host for this page is {host}</h1>
+               <h2>The name of this application is {appname}</h2>
+               <h3>The path of this application on the user's device is {g.path}</h3>
+               ''', 200, {})
+    return response
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
